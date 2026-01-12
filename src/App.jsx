0297d58bbb87947b49ce0c4d0546f78cdaa911af
Elizabeth Hernandez <418dianahernandz@gmail.com>
2026-01-12 10:30:29 -0600

    feature: se agrega rutas al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
-import Board from './components/Board'
+import {Routes, Route} from 'react-router-dom'
+import {Board, PhoneBook} from './components'
+
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div style={{ width: '60vw' }}>
-        <Board></Board>
-      </div>
-    </>
+  <div style={{ width: '60vw' }}>
+    <Routes>
+      <Route index element={<Board />} />
+      <Route path="/tic-tac-play" element ={<Board/>} />
+      <Route path="/phone-book-play" element ={<PhoneBook/>} />
+      <Route path="*" element={<h1>404 - Not Found</h1>} />
+    </Routes>
+  </div>
   )
 }
 
---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as Board } from './Board';
+export { default as PhoneBook } from './PhoneBook';
+export {default as Square } from './Square';
